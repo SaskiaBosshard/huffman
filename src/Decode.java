168efe9d71bdd78b45001e273a730ff1924cffde
Saskia Bosshard <saskia.bosshard@students.fhnw.ch>
2023-05-20 23:34:06 +0200

    added method collection to decode. Encode and decode need debugging

--- src/Decode.java
+++ src/Decode.java
@@ -1,8 +1,86 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+/**
+ * Decoding of input file in huffman coding and writing output into new file.
+ */
 public class Decode {
-    public void decode(Path compressedInputPath, Path huffmanPath, Path decompressedInputPath) {
+    public void decode(Path compressedHuffmanInput, Path huffmanCodeCollection, Path decompressedDataOutput) throws IOException{
+        byte[] bytes = fileToByteArray(compressedHuffmanInput);
+
+        String bitString = getBitString(bytes);
+        bitString = everythingUpToLastOne(bitString);
+
+        Map<String, Character> code = loadCodeCollection(Files.readString(huffmanCodeCollection, StandardCharsets.US_ASCII).trim());
+        String decoded = decodeBitString(bitString, code);
+
+        Files.writeString(decompressedDataOutput, decoded);
+
+    }
+    /**
+     * Read file into byte array and return it.
+     */
+    public byte[] fileToByteArray(Path compressedHuffmanInput) throws IOException {
+        return Files.readAllBytes(compressedHuffmanInput);
     }
 
+    /**
+     * Convert byte array to bit string.
+     */
+    public String getBitString(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
 
+        for (byte aByte : bytes) {
+            String bitString = Integer.toBinaryString(Byte.toUnsignedInt(aByte));
+
+            // Putting missing leading 0s back in the front
+            bitString = String.format("%8s", bitString).replace(' ', '0');
+            sb.append(bitString);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Cut off last 1 and everything after from a bit string.
+     */
+    public String everythingUpToLastOne(String bitString) {
+        return bitString.substring(0, bitString.lastIndexOf("1"));
+    }
+
+    /**
+     * Converts huffman code collection string into map.
+     * The string format:
+     * {ascii decimal value}:{path}-{ascii decimal value}:{path}
+     * e.g.: 10:1010111-13:1010110
+     */
+    public Map<String, Character> loadCodeCollection(String codeCollection) {
+        String[] codesFromCollection = codeCollection.split("-");
+
+        return Arrays.stream(codesFromCollection)
+                .map(x -> x.split(":"))
+                .collect(Collectors.toMap(x -> x[1], x -> (char) Integer.valueOf(x[0]).intValue()));
+    }
+
+    /**
+     * Decode the given bit string with a huffman code collection.
+     */
+    public String decodeBitString(String inputString, Map<String, Character> codeCollection) {
+        StringBuilder match = new StringBuilder();
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < inputString.length(); i++) {
+            match.append(inputString.charAt(i));
+            if (codeCollection.containsKey(match.toString())) {
+                result.append(codeCollection.get(match.toString()));
+                match = new StringBuilder();
+            }
+        }
+
+        return result.toString();
+    }
 }
--- src/Encode.java
+++ src/Encode.java
@@ -1,4 +1,4 @@
-//import javax.swing.tree.TreeNode;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -10,24 +10,21 @@ import java.util.PriorityQueue;
 import java.util.stream.Collectors;
 
 /**
- * Methods to encode data with huffman encoding.
+ * Encoding of input file in huffman coding and writing output into new file.
  */
 public class Encode {
     public void encode(Path asciiInputPath, Path huffmanDataFile, Path codeCollection) throws IOException {
         String inputFromFile = readFile(asciiInputPath);
+
         int[] frequencyTable = getFrequencyTable(inputFromFile);
         TreeNode huffmanTree = createHuffmanTree(frequencyTable);
         Map<Character, String> code = createHuffmanCode(huffmanTree);
-        writeHuffmanToFile(code, codeCollection);
-        //todo
-        //createBitString(code, input);
-                //Create bit string from ascii input and a huffman code collection.
-                //Encoded bit string, with an additional 1 and filled up with 0 until the next multiple of 8.
-        //createByteArray(bitString); bitString must be multiple of 8
-                //8 consecutive characters are 1 byte
-        //writeData(bytes, huffmanDataFile); huffmanDataFile being the output file
-    }
 
+        writeHuffmanToFile(code, codeCollection);
+        String bitString = createBitString(code, inputFromFile);
+        byte[] b = toByteArray(bitString);
+        writeData(b, huffmanDataFile);
+    }
 
     /**
      * Read a file to a string.
@@ -38,12 +35,11 @@ public class Encode {
 
     /**
      * Create a frequency table of every char from input String
-     * In total there are 128 ascii chars, so length of array = 128.
+     * Length of array = number of ascii chars = 128.
      * The index represents the character (ASCII value) and the value
      * at that index represents the frequency of that character in
      * the input string.
-     *
-     * @param input an ascii String
+
      * @return streamlined frequency table
      */
 
@@ -67,8 +63,7 @@ public class Encode {
      * The algorithm is taken from this source:
      * https://www.techiedelight.com/huffman-coding/
      *
-     * @param fTable Frequency table of the ascii characters of an input String.
-     * @return root of the created tree
+     * @return root TreeNode of the created tree
      */
 
     public static TreeNode createHuffmanTree(int[] fTable) {
@@ -101,7 +96,7 @@ public class Encode {
     }
 
     /**
-     * Create huffman code from a previously generates Huffman tree.
+     * Create huffman code from a previously created Huffman tree.
      * Left branches adds a 0 and right branches a 1.
      * An ascii-Character and its specific path are added to the code collection
      * when they are a leaf (treeNode with no left or right node).
@@ -116,9 +111,6 @@ public class Encode {
 
     /**
      * Method which allows to trace along the Huffman tree
-     * @param map  Huffman code collection / path of the ascii-character
-     * @param rootOfTree Respective root node.
-     * @param way  The path so far.
      */
     private void traceTree(Map<Character, String> map, TreeNode rootOfTree, String way) {
         if (rootOfTree.left == null && rootOfTree.right == null) {
@@ -132,9 +124,6 @@ public class Encode {
     /**
      * Writes a huffman code collection to a file.
      * Example of the created format: 10:1010111-13:1010110
-     *
-     * @param code    Huffman code book of the form character -> path
-     * @param outPath Path where the Huffman code collection is written to.
      */
     public void writeHuffmanToFile(Map<Character, String> code, Path outPath) throws IOException {
         String output = code.keySet().stream()
@@ -144,5 +133,53 @@ public class Encode {
 
         Files.writeString(outPath, output, StandardCharsets.US_ASCII);
     }
+
+    /**
+     * Creates a bit string from ascii input file and a huffman code collection.
+     * @return Encoded bit string, with an additional 1 and filled up with 0 until the next multiple of 8
+     */
+    public String createBitString(Map<Character, String> code, String inputFromFile) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < inputFromFile.length(); i++) {
+            char charFromFile = inputFromFile.charAt(i);
+            String value = code.get(charFromFile);
+            sb.append(value);
+        }
+        sb.append("1");
+
+        int zeroLength = 8 - sb.length() % 8;
+        for (int i = 0; i < zeroLength; i++) {
+            sb.append("0");
+        }
+
+        //assert (sb.length() % 8 == 0); // simple test, it always has to be a multiple of BYTE_LENGTH.
+
+        return sb.toString();
+    }
+
+    /**
+     * Creates a byte array from a bit string.
+     * @return Byte array which represents the input bit string.
+     */
+    public byte[] toByteArray(String bitString) {
+        int numberOfBytes = bitString.length() / 8;
+        byte[] byteArray = new byte[numberOfBytes];
+
+        for (int i = 0; i < bitString.length(); i += 8) {
+            String bits = bitString.substring(i, i + 8);
+            Integer x = Integer.parseInt(bits, 2);
+            byte b = x.byteValue();
+            byteArray[i / 8] = b;
+        }
+        return byteArray;
+    }
+
+    public void writeData(byte[] data, Path outputPath) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(outputPath.toString())) {
+            fos.write(data);
+        }
+    }
+
 
 }
